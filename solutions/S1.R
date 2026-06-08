7b9384d0d9c8fb5769a5d3308f25f0f7de49b9b6
ineswilms <i.wilms@maastrichtuniversity.nl>
2026-06-08 16:56:18 +0200

    solutions session1

--- solutions/S1.R
+++ solutions/S1.R
@@ -1,10 +1,15 @@
 # Solutions Session 1
 
 # Exercise 1.7
-library('rio')
-import("data/climate_wide.dta")
+library(rio)
 import("data/climate.csv")
 
 # Exercise 1.8
 save.image() # this saves your current workspace
-save(AAPL, "data/AAPL.RData") # saving a specific object
+
+library(quantmod)
+# Get Apple Inc. (AAPL) stock data from Yahoo Finance
+getSymbols("AAPL", src = "yahoo", from = "2024-01-01", to = "2025-06-13")
+# View the data
+View(AAPL)
+save(AAPL, file = "data/AAPL.RData") # saving a specific object
