2c668e6f448a3d30e2a054460b08f24bec3b03e1
ineswilms <i.wilms@maastrichtuniversity.nl>
2026-06-13 08:59:52 +0200

    update session 5

--- solutions/S5.R
+++ solutions/S5.R
@@ -2,31 +2,6 @@
 data_grades <- read.table("data/grades.csv", 
 													header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
-# # Exercise 5.1
-# data_sub <- data_grades[, c('ID', 'Name', 'Tutorial',   'Participation_Grade', 'Exam_Score')]
-# data_sub_4 <- data_sub[data_sub$Tutorial == 4, ]
-# data_sub_4_exam <- data_sub_4[data_sub_4$Exam_Score > 80, ]
-# nrow(data_sub_4_exam)
-# data_sub_4_exam
-# # Alternatively in one go:
-# # data_sub[(data_sub$Tutorial == 4) & (data_sub$Exam_Score > 80), ]
-# 
-# # Exercise 5.2
-# class(data_grades$Tutor)
-# data_grades$Tutor <- as.factor(data_grades$Tutor)
-# levels(data_grades$Tutor)
-# 
-# data_grades$Final_Score <- 0.2*data_grades$Participation_Grade + 0.08*data_grades$Exam_Score
-# # Alternative, once you added the variable Exam_Score_10 to your data set:
-# # data_grades$Exam_Score_10 <- data_grades$Exam_Score/10
-# # data_grades$Final_Score <- 0.2*data_grades$Participation_Grade + 0.8*data_grades$Exam_Score_10
-# 
-# data_2 <- data_grades[data_grades$Tutorial==2, ]
-# summary(data_2$Final_Score)
-# 
-# min(data_2$Final_Score)
-# max(data_2$Final_Score)
-
 # Exercise 5.1
 data_grades$Final_Score <- 0.2*data_grades$Participation_Grade + 0.08*data_grades$Exam_Score
 my_reg1 <- lm(Final_Score ~ Participation_Grade, data = data_grades)
